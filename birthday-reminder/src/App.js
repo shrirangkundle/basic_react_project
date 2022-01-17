fb13f0ab148313d175f865740cc75bbd1db2562c
Shrirang <shrirangkundle10@gmail.com>
2022-01-17 14:18:49 +0530

    birthday reminder completion and tours creation

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
-    <main>
-      <section className="container">
-        <h3>0 Birthday today</h3>
-        <List />
-        <button onClick={() => console.log("you clicked me ")}>
-          Clear all
-        </button>
-      </section>
-    </main>
+    <>
+      <h1 style={{ textAlign: "center" }}>Birthday List </h1>
+      <main>
+        <section className="container">
+          <h3>{people.length} Birthday today</h3>
+          <List people={people} />
+          <button onClick={() => setPeople([])}>Clear all</button>
+        </section>
+      </main>
+    </>
   );
 }
 
