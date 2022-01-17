1bdfee26f85b66b71931b55a2fdc342cdef01e69
Shrirang <shrirangkundle10@gmail.com>
2022-01-17 15:22:19 +0530

    tour deletion

--- tour-listing/src/App.js
+++ tour-listing/src/App.js
@@ -7,6 +7,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -31,9 +36,23 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <>
+        <main>
+          <div className="title">
+            <h2>No Tours Left</h2>
+            <button className="btn" onClick={fetchTours}>
+              refresh
+            </button>
+          </div>
+        </main>
+      </>
+    );
+  }
   return (
     <main>
-      <Tours tours={tours}></Tours>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
     </main>
   );
 }
