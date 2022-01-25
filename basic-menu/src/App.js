f196e757d99480d7f47f79adce3290fdb0663c06
Shrirang <shrirangkundle10@gmail.com>
2022-01-26 04:46:54 +0530

    basic menu completed

--- basic-menu/src/App.js
+++ basic-menu/src/App.js
@@ -3,16 +3,32 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <section className="menu section">
         <div className="title">
           <h2>Our Menu</h2>
           <div className="underline"></div>
-          <Categories></Categories>
+          <Categories
+            categories={categories}
+            filterItems={filterItems}
+          ></Categories>
           <Menu items={menuItems}></Menu>
         </div>
       </section>
