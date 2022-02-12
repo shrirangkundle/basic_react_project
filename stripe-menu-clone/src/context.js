cd33cb74bfe7f79ab4a8619ba4e151f5c9f5dee1
Shrirang <shrirangkundle10@gmail.com>
2022-02-12 12:43:27 +0530

    setting up custom hook

--- stripe-menu-clone/src/context.js
+++ stripe-menu-clone/src/context.js
@@ -1,2 +1,43 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const opeSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+        opeSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+//custom hook
+
+export const useGlobaclContext = () => {
+  return useContext(AppContext);
+};
