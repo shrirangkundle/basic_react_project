106fb334b43ee8c00536509ccf0434c1864b9b55
Shrirang <shrirangkundle10@gmail.com>
2022-02-12 18:16:03 +0530

    basic setup done for cocktail menu

--- cocktail-menu/src/components/CocktailList.js
+++ cocktail-menu/src/components/CocktailList.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">No Cocktails matched your search</h2>;
+  }
   return (
     <div>
       <h2>cocktail list component</h2>
     </div>
-  )
-}
+  );
+};
 
-export default CocktailList
+export default CocktailList;
--- cocktail-menu/src/pages/About.js
+++ cocktail-menu/src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit vel
+        pariatur similique labore corrupti quo beatae sequi eos, dolore
+        excepturi asperiores praesentium ea id a impedit eius numquam, maiores
+        adipisci! Pariatur corporis necessitatibus quisquam reiciendis quibusdam
+        possimus molestias iste, perspiciatis eaque dolore dignissimos mollitia,
+        ea inventore impedit asperiores. Possimus, voluptatibus?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
