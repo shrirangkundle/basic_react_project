34739adbd75e5dcc227e4b9d0454a0b935d3f991
Shrirang <shrirangkundle10@gmail.com>
2022-02-12 17:17:21 +0530

    api integration

--- cart-clone/src/reducer.js
+++ cart-clone/src/reducer.js
@@ -34,15 +34,26 @@ const reducer = (state, action) => {
   }
 
   if (action.type === "GET_TOTALS") {
-    const { total, amount } = state.cart.reduce(
+    let { total, amount } = state.cart.reduce(
       (cartTotal, cartItem) => {
         const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.total += itemTotal;
         cartTotal.amount += amount;
         return cartTotal;
       },
       { total: 0, amount: 0 }
     );
+    total = parseFloat(total.toFixed(2));
     return { ...state, total, amount };
+  }
+
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
   }
 
   return state;
